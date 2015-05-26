b020edc98844f9480c893ef0addbf1d89a9b0802
Alexey Vlaskin <alex@avlaskin.com>
2015-05-26 20:08:35 +1000

    Initial Commit

---
+++ AVRegression/AVRegression.h
@@ -0,0 +1,13 @@
+//
+//  AVRegression.h
+//  AVRegression
+//
+//  Created by Alexey Vlaskin on 26/05/2015.
+//  Copyright (c) 2015 Alexey Vlaskin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AVRegression : NSObject
+
+@end
---
+++ AVRegression/AVRegression.m
@@ -0,0 +1,13 @@
+//
+//  AVRegression.m
+//  AVRegression
+//
+//  Created by Alexey Vlaskin on 26/05/2015.
+//  Copyright (c) 2015 Alexey Vlaskin. All rights reserved.
+//
+
+#import "AVRegression.h"
+
+@implementation AVRegression
+
+@end
